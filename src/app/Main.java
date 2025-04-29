50cb188ff50cd2309c1ada34e8b87a8bd4c37bab
Miroslav <myroslav.staryk@viti.edu.ua>
2025-04-29 22:20:45 +0300

    Initial commit: create Java project structure

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,4 +1,39 @@
 package app;
 
+import java.util.Scanner;
+
 public class Main {
+    static double balance;
+
+    public static void main(String[] args) {
+        balance = getBalance();
+        validateAmount(balance, getAmount());
+    }
+
+    private static double getBalance() {
+        return 1000.00;
+    }
+
+    private static double getAmount() {
+        System.out.printf("Balance is USD %.2f.%n Enter purchase amount, USD: ", balance);
+        Scanner scanner = new Scanner(System.in);
+        return scanner.nextDouble();
+    }
+
+    private static void validateAmount(double balance, double withdrawal) {
+        if (balance > withdrawal) {
+            try {
+                throw new FundsException("Insufficient funds!");
+            } catch (FundsException ex) {
+                System.out.println(ex.getMessage());
+            }
+        } else {
+            balance = updateBalance(balance, withdrawal);
+            System.out.printf("Funds are OK. Purchase paid.%nBalance is USD %.2f%n\", balance");
+        }
+    }
+
+    private static double updateBalance(double balance, double withdrawal) {
+        return balance - withdrawal;
+    }
 }
